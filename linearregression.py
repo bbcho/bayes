3d75000e18c5ad2597852a410d32e0b28d5e9170
Ben Cho <ben.cho@gmail.com>
2023-05-29 21:14:56 -0600

    bayes working

--- linearregression.py
+++ linearregression.py
@@ -4,9 +4,10 @@ a = 2.3
 b = 4.1
 mu = 0
 sd = 12.2
+size = 200
 
 rng = np.random.RandomState(13)
 
-x = rng.uniform(0,10, size=100)
+x = rng.uniform(0,10, size=size)
 
-y = a + b*x + rng.normal(mu, sd, size=100) 
+y = a + b*x + rng.normal(mu, sd, size=size) 
